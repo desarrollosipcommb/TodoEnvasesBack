7c78c0f857e3f5f4c9bfe94af9c26ef4dc2af1a4
Felipe Barreto <pipe.barreto07@gmail.com>
2025-07-16 16:01:07 -0500

    fix: se borraron algunos imports innecesarios

--- src/main/java/com/sipcommb/envases/repository/InventoryRepository.java
+++ src/main/java/com/sipcommb/envases/repository/InventoryRepository.java
@@ -3,9 +3,8 @@ package com.sipcommb.envases.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.sipcommb.envases.entity.ItemType;
 import com.sipcommb.envases.entity.Transaction;
-import com.sipcommb.envases.entity.TransactionType;
+
 
 @Repository
 public interface InventoryRepository extends JpaRepository<Transaction, Long>{
